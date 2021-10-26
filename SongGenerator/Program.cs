286bf3d4102bc46fd4ab763dc549b838cd675605
Sp00c <cooper.leejohn@gmail.com>
2021-10-26 19:25:14 +0100

    Added PlaySongFunc

--- SongGenerator/Program.cs
+++ SongGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using SongGenerator;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using static SongGenerator.Generator;
 
 namespace SongGenerator
 {
@@ -8,18 +10,16 @@ namespace SongGenerator
     {
         static void Main(string[] args)
         {
+            static void PlaySong(IEnumerable<(int, int)> x) => x.ToList().ForEach(s => Console.Beep(s.Item1, s.Item2));
+
             string[] jingleBellsNotes = { "B|4|4", "B|4|4", "B|4|2", "B|4|4", "B|4|4", "B|4|2", "B|4|4", "D|5|4", "G|4|4", "A|4|4", "B|4|1" };
             string[] doTheyKnowNotes = { "E|4|2", "A|3|2", "F#|4|2", "E|4|2", "F#|4|4", "E|4|4", "D|4|4", "C#|4|4", "D|4|4", "C#|4|4", "B|3|4", "A|3|2" };
-            string[] goodKingWenceslasNotes = { "E|4|4", "E|4|4", "B|4|4", "B|4|4", "A|4|4", "G|4|4", "F#|4|4", "E|4|4", "D|4|4", "E|4|4", "F#|4|4", "G|4|4", "A|4|4", "B|4|1" };
+            string[] merryGentlemenNotes = { "E|4|4", "E|4|4", "B|4|4", "B|4|4", "A|4|4", "G|4|4", "F#|4|4", "E|4|4", "D|4|4", "E|4|4", "F#|4|4", "G|4|4", "A|4|4", "B|4|1" };
 
             var noteService = new NoteService();
-            var jingleBellsSong = Generator.CreateSong(jingleBellsNotes, noteService).ToList();
-            var doTheyKnowSong = Generator.CreateSong(doTheyKnowNotes, noteService).ToList();
-            var goodKingWenceslasSong = Generator.CreateSong(goodKingWenceslasNotes, noteService).ToList();
-
-            goodKingWenceslasSong.ForEach(n => Console.Beep(n.Item1, n.Item2));
-            doTheyKnowSong.ForEach(n => Console.Beep(n.Item1, n.Item2));
-            jingleBellsSong.ForEach(n => Console.Beep(n.Item1, n.Item2));
+            PlaySong(CreateSong(jingleBellsNotes, noteService));
+            PlaySong(CreateSong(doTheyKnowNotes, noteService));
+            PlaySong(CreateSong(merryGentlemenNotes, noteService));
         }
     }
 }
